31b644c0685f7c5a813b2981199433b47bd2de28
lcy <lcy@sc.10086.cn.defaultbadlist>
2017-10-04 18:26:06 +0800

    Initial Commit

---
+++ AHelloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AHelloworld
+//
+//  Created by lcy on 2017/10/4.
+//  Copyright © 2017年 lcy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
